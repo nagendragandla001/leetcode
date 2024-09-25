3cda079d5558acc3fd2538b9b5e79cbbe5800f57
Nagendra Gandla <nagendragandla003@gmail.com>
2024-09-25 19:42:55 +0530

    equalpair

--- 39.equalPairs.js
+++ 39.equalPairs.js
@@ -2,32 +2,40 @@
  * @param {number[][]} grid
  * @return {number}
  */
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
 var equalPairs = function (grid) {
-  const columns = new Map();
-  const rows = new Map();
-  let rowNums, colNums, rowHash, colHash;
-  let ans = 0;
-
-  for (let i = 0; i < grid.length; i++) {
-    rowNums = [];
-    colNums = [];
-
-    for (let j = 0; j < grid[i].length; j++) {
-      rowNums.push(grid[i][j]);
-      colNums.push(grid[j][i]);
+  let convertToKey = (arr) => {
+    let key = '';
+    for (const num of arr) {
+      key += num + ',';
     }
 
-    rowHash = rowNums.join(',');
-    colHash = colNums.join(',');
+    return key;
+  };
 
-    rows.set(rowHash, (rows.get(rowHash) || 0) + 1);
-    columns.set(colHash, (columns.get(colHash) || 0) + 1);
+  let dic = new Map();
+  for (const arr of grid) {
+    let key = convertToKey(arr);
+    dic.set(key, (dic.get(key) || 0) + 1); // B
   }
 
-  for (const [row, value] of rows) {
-    if (columns.has(row)) {
-      ans += value * columns.get(row);
+  let dic2 = new Map();
+  for (let col = 0; col < grid[0].length; col++) {
+    let currentCol = [];
+    for (let row = 0; row < grid.length; row++) {
+      currentCol.push(grid[row][col]); // C
     }
+
+    let key = convertToKey(currentCol);
+    dic2.set(key, (dic2.get(key) || 0) + 1);
+  }
+
+  let ans = 0;
+  for (const [key, val] of dic) {
+    ans += val * dic2.get(key) || 0; // A
   }
 
   return ans;
@@ -43,6 +51,6 @@ const input1 = [
   [2, 4, 2, 2],
   [2, 4, 2, 2],
 ];
-const result = equalPairs(input);
+const result = equalPairs(input1);
 
 console.log(result); // 1
